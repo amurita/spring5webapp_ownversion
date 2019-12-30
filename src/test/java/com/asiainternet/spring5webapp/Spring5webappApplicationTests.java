c549b2317445dd6380f8750d644cf7a0813d12b6
Amurita <amurita.dev@gmail.com>
2019-12-30 18:15:02 +0100

    Initial commit.

---
+++ src/test/java/com/asiainternet/spring5webapp/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asiainternet.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
